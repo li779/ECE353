cdeb9d3675fd8fcca0c7e38ac3a55f99faa50088
MarvinZhang0126 <MarvinZhang0126@hotmail.com>
2020-04-18 13:52:25 -0500

    Added file io_expander.c.

---
+++ peripherals/c/io_expander.c
@@ -0,0 +1,17 @@
+#include "io_expander.h"
+
+bool io_expander_init(void)
+{
+
+    return false;
+}
+
+void io_expander_write_reg(uint8_t reg, uint8_t data)
+{
+    return;
+}
+
+uint8_t io_expander_read_reg(uint8_t)
+{
+    return 0;
+}
