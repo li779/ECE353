784a556803e05899dad567adaa65d91e90d6ceb4
MarvinZhang0126 <MarvinZhang0126@hotmail.com>
2020-04-24 22:45:59 -0500

    Changed part of project_hardware_init.c

--- Project/breathing_led.c
+++ Project/breathing_led.c
@@ -1,1 +1,14 @@
 #include "breathing_led.h"
+
+
+
+void breathing_led_init(void)
+{
+	lp_io_init();
+	
+	// Allow Timer1 to send interrupts out, but set a low priority
+	NVIC_SetPriority(TIMER1A_IRQn, 5);
+	NVIC_EnableIRQ(TIMER1A_IRQn);
+	
+	
+}
--- Project/breathing_led.h
+++ Project/breathing_led.h
@@ -2,3 +2,14 @@
 #define __BREATHING_LED_H__
 #endif
 
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "TM4C123.h"
+#include "driver_defines.h"
+#include "launchpad_io.h"
+#include "gpio_port.h"
+
+void breathing_led_init(void);
